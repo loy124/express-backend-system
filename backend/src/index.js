b2682de70b459aa3ac3cfae40e6344221e31bc2c
mincoding-onyou <72716972+mincoding-onyou@users.noreply.github.com>
2021-07-12 13:38:28 +0900

    keep alive 위치 변경하기

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,14 @@ const PORT = 8080;
 const { stream } = require("./utils/logger.js");
 const morgan = require("morgan");
 const routes = require("./routes");
-const app = express();
+const app = new express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("combined", { stream }));
+
+let isDisableKeepAlive = false;
+
 app.use(function (req, res, next) {
   if (isDisableKeepAlive) {
     res.set("Connection", "close");
@@ -16,16 +19,14 @@ app.use(function (req, res, next) {
 });
 app.use(routes);
 
-let isDisableKeepAlive = false;
 
 const server = app.listen(PORT, () => {
   process.send("ready");
   console.log(`this server listening on ${PORT}`);
 });
-
 process.on("SIGINT", function () {
   isDisableKeepAlive = true;
-
+  
   server.close(function () {
     console.log("server closed");
     process.exit(0);
